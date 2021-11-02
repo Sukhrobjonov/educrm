2d04c9ab3e3b73b7607e2809c8e1c149f01a28b9
Javohir <javohir200903@gmail.com>
2021-11-02 18:32:21 +0500

    add custom error class

--- src/modules/pg/postgres.js
+++ src/modules/pg/postgres.js
@@ -1,10 +1,14 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_URL);
+const sequelize = new Sequelize(process.env.DB_URL, {
+    logging: false,
+});
 
 module.exports = async function postgres() {
     try {
         await sequelize.authenticate();
+        const db = {};
+        return db;
     } catch (error) {
         console.log(`DATABASE ERROR: ${error}`);
     }
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const PORT = process.env.PORT || 5000;
 const morgan = require("morgan");
+const databaseMiddleware = require("./middlewares/databaseMiddleware");
+const postgres = require("./modules/pg/postgres");
 const app = express();
 
 async function server(mode) {
@@ -10,6 +12,11 @@ async function server(mode) {
         });
         app.use(express.json());
         app.use(express.urlencoded({ extended: true }));
+        const db = await postgres();
+
+        await databaseMiddleware(db, app);
+
+        // console.log(db);
 
         if (mode == "dev") app.use(morgan("dev"));
     } catch (error) {
