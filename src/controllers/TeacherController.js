78afaa6ab3279f165bb538b33b8c4a36b0a361f2
Javohir <javohir200903@gmail.com>
2021-11-03 02:07:06 +0500

    add teacherController

---
+++ src/controllers/TeacherController.js
@@ -0,0 +1,9 @@
+module.exports = class TeacherController {
+    static async AddTeacherPostController(req, res, next) {
+        try {
+            //
+        } catch (error) {
+            next(error);
+        }
+    }
+};
