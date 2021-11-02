113a08de21122346968c522ac27bd32f9b61c002
Javohir <javohir200903@gmail.com>
2021-11-03 04:17:09 +0500

    add CourseController

---
+++ src/controllers/CourseController.js
@@ -0,0 +1,9 @@
+module.exports = class CourseController {
+    static async CourseCreatePostController(req, res, next) {
+        try {
+            //
+        } catch (error) {
+            next(error);
+        }
+    }
+};
