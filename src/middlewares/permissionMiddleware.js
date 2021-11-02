a655d1493b028b689ad282173ff856f6dc009a4a
Javohir <javohir200903@gmail.com>
2021-11-02 23:55:19 +0500

    add admin permission

---
+++ src/middlewares/permissionMiddleware.js
@@ -0,0 +1,3 @@
+module.exports = async function PermissionMiddleware(req, res, next) {
+    console.log(req.sessions);
+};
--- src/routes/users/UserRoute.js
+++ src/routes/users/UserRoute.js
@@ -3,11 +3,16 @@ const {
     CreateUserPostController,
 } = require("../../controllers/UserController");
 const authMiddleware = require("../../middlewares/authMiddleware");
+const permissionMiddleware = require("../../middlewares/permissionMiddleware");
 
 const router = require("express").Router();
 
 router.post("/sign_in", SignInPostController);
-router.post("/account", [authMiddleware], CreateUserPostController);
+router.post(
+    "/account",
+    [authMiddleware, permissionMiddleware],
+    CreateUserPostController
+);
 
 module.exports = {
     path: "/users",
