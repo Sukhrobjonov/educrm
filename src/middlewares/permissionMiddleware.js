9175b707028870eff92ffdec8be81e25e54b2f78
Javohir <javohir200903@gmail.com>
2021-11-03 00:20:34 +0500

    add permissionMiddleware

--- src/middlewares/permissionMiddleware.js
+++ src/middlewares/permissionMiddleware.js
@@ -1,3 +1,14 @@
 module.exports = async function PermissionMiddleware(req, res, next) {
-    console.log(req.sessions);
+    try {
+        const permission = await req.db.user_permissions.findAll({
+            where: { user_id: req.sessions.user_id },
+            include: req.db.permissions,
+            raw: true,
+        });
+
+        req.user_permissions = permission;
+        next();
+    } catch (error) {
+        next(error);
+    }
 };
