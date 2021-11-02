fa148fdfa191611203fd399b5bc4a5825cb72a1c
Javohir <javohir200903@gmail.com>
2021-11-02 23:39:31 +0500

    add permission and user_permission realtion

--- src/modules/pg/relations.js
+++ src/modules/pg/relations.js
@@ -12,4 +12,32 @@ module.exports = async function (db) {
             allowNull: false,
         },
     });
+
+    await db.users.hasMany(db.user_permissions, {
+        foreignKey: {
+            name: "user_id",
+            allowNull: false,
+        },
+    });
+
+    await db.user_permissions.belongsTo(db.users, {
+        foreignKey: {
+            name: "user_id",
+            allowNull: false,
+        },
+    });
+
+    await db.permissions.hasMany(db.user_permissions, {
+        foreignKey: {
+            name: "permission_id",
+            allowNull: false,
+        },
+    });
+
+    await db.user_permissions.belongsTo(db.permissions, {
+        foreignKey: {
+            name: "permission_id",
+            allowNull: false,
+        },
+    });
 };
