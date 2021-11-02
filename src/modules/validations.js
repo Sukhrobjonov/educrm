666d9f7a0be21d3bb383b8513fdc929390a2f559
Javohir <javohir200903@gmail.com>
2021-11-02 19:50:09 +0500

    add signup and signin validation

---
+++ src/modules/validations.js
@@ -0,0 +1,50 @@
+const joi = require("joi");
+
+module.exports = class Validations {
+    static async SignUpValidation(data, error) {
+        return await joi
+            .object({
+                name: joi
+                    .string()
+                    .min(5)
+                    .max(64)
+                    .required()
+                    .error(new error(400, "Name is invalid")),
+                username: joi
+                    .string()
+                    .regex(/^[A-Za-z]{2,}[_-]?[A-Za-z0-9]{2,}$/)
+                    .required()
+                    .error(new error(400, "Username is invalid")),
+                password: joi
+                    .string()
+                    .min(5)
+                    .max(128)
+                    .required()
+                    .error(new error(400, "Password is invalid")),
+                gender: joi
+                    .string()
+                    .valid("male", "female")
+                    .required()
+                    .error(new error(400, "This option isn't available")),
+            })
+            .validateAsync(data);
+    }
+
+    static async SignInValidation(data, error) {
+        return await joi
+            .object({
+                username: joi
+                    .string()
+                    .regex(/^[A-Za-z]{2,}[_-]?[A-Za-z0-9]{2,}$/)
+                    .required()
+                    .error(new error(400, "Username is invalid")),
+                password: joi
+                    .string()
+                    .min(5)
+                    .max(128)
+                    .required()
+                    .error(new error(400, "Password is invalid")),
+            })
+            .validateAsync(data);
+    }
+};
