12a74c15bc84abcd800a7db2433b0b23e427ef94
Javohir <javohir200903@gmail.com>
2021-11-02 18:42:04 +0500

    add routes

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const customErrorMiddleware = require("./middlewares/customErrorMiddleware");
 const databaseMiddleware = require("./middlewares/databaseMiddleware");
 const postgres = require("./modules/pg/postgres");
+const routes = require("./routes/routes");
 const app = express();
 
 async function server(mode) {
@@ -11,19 +12,19 @@ async function server(mode) {
         app.listen(PORT, () => {
             console.log(`Server started at ${PORT} port`);
         });
+
+        const db = await postgres();
+        await databaseMiddleware(db, app);
+        app.use(customErrorMiddleware);
+
         app.use(express.json());
         app.use(express.urlencoded({ extended: true }));
-        const db = await postgres();
-
-        await databaseMiddleware(db, app);
-
-        app.use(customErrorMiddleware);
 
         if (mode == "dev") app.use(morgan("dev"));
     } catch (error) {
         console.log(`SERVER ERROR: ${error}`);
     } finally {
-        //
+        routes(app);
     }
 }
 
