94cbdae4f1a594bce151503f7e40054dd6406c16
Javohir <javohir200903@gmail.com>
2021-11-02 20:23:23 +0500

    add user and session relation

--- src/modules/pg/postgres.js
+++ src/modules/pg/postgres.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require("sequelize");
 const SessionModel = require("../../models/SessionModel");
 const UserModel = require("../../models/UserModel");
+const relations = require("./relations");
 
 const sequelize = new Sequelize(process.env.DB_URL, {
     logging: false,
@@ -9,9 +10,14 @@ const sequelize = new Sequelize(process.env.DB_URL, {
 module.exports = async function postgres() {
     try {
         await sequelize.authenticate();
+
         const db = {};
+
         db.users = await UserModel(sequelize, Sequelize);
         db.sessions = await SessionModel(sequelize, Sequelize);
+
+        relations(db);
+
         return db;
     } catch (error) {
         console.log(`DATABASE ERROR: ${error}`);
