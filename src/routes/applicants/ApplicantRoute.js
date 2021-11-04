793e575f8b0e0f6a6adaa0358fcf7efd07044681
Javohir <javohir200903@gmail.com>
2021-11-04 20:56:07 +0500

    add applicantRoute

---
+++ src/routes/applicants/ApplicantRoute.js
@@ -0,0 +1,11 @@
+const authMiddleware = require("../../middlewares/authMiddleware");
+const permissionMiddleware = require("../../middlewares/permissionMiddleware");
+
+const router = require("express").Router();
+
+router.use([authMiddleware, permissionMiddleware]);
+
+module.exports = {
+    path: "/applicants",
+    router,
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,5 @@
 const errorHandler = require("../helpers/errorHandler");
+const ApplicantRoute = require("./applicants/ApplicantRoute");
 const CourseRoute = require("./courses/CourseRoute");
 const TeacherRoute = require("./teachers/TeacherRoute");
 const UserRoute = require("./users/UserRoute");
@@ -8,6 +9,7 @@ module.exports = (app) => {
         app.use(UserRoute.path, UserRoute.router);
         app.use(TeacherRoute.path, TeacherRoute.router);
         app.use(CourseRoute.path, CourseRoute.router);
+        app.use(ApplicantRoute.path, ApplicantRoute.router);
     } finally {
         app.use(errorHandler);
     }
