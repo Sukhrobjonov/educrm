020a1211c14ebaabf2a96e5db312f7eb480f8459
Javohir <javohir200903@gmail.com>
2021-11-15 04:28:58 +0500

    add GroupStudentsGetAllo

--- src/controllers/GroupStudentController.js
+++ src/controllers/GroupStudentController.js
@@ -44,7 +44,7 @@ module.exports = class GroupStudentController {
         }
     }
 
-    static async GroupUpdatePutController(req, res, next) {
+    static async GroupStudentsGetAllController(req, res, next) {
         try {
             permissionChecker(
                 ["admin", "teacher", "operator"],
@@ -52,112 +52,15 @@ module.exports = class GroupStudentController {
                 res.error
             );
 
-            const group_id = req.params.group_id;
-
-            const group = await req.db.groups.findOne({
-                where: {
-                    group_id,
-                },
+            const group_students = await req.db.group_students.findAll({
                 raw: true,
             });
-
-            if (!group) throw new res.error(404, "Group not found");
-
-            const data = await GroupCreateValidation(req.body, res.error);
-
-            const course = await req.db.courses.findOne({
-                where: {
-                    course_id: data.course_id,
-                },
-                raw: true,
-            });
-
-            if (!course) throw new res.error(404, "Course not found");
-
-            const teacher = await req.db.teachers.findOne({
-                where: {
-                    teacher_id: data.teacher_id,
-                },
-                raw: true,
-            });
-
-            if (!teacher) throw new res.error(404, "Teacher not found");
-
-            await req.db.groups.update(
-                {
-                    group_time: data.time,
-                    group_status: data.status,
-                    group_schedule: data.schedule,
-                    group_lesson_duration: data.lesson_duration,
-                    group_course_duration: data.course_duration,
-                    teacher_id: data.teacher_id,
-                    course_id: data.course_id,
-                },
-                {
-                    where: {
-                        group_id,
-                    },
-                }
-            );
 
             res.status(200).json({
                 ok: true,
-                message: "Group updated successfully",
-            });
-        } catch (error) {
-            next(error);
-        }
-    }
-
-    static async GroupGetOneController(req, res, next) {
-        try {
-            permissionChecker(
-                ["admin", "teacher", "operator"],
-                req.user_permissions,
-                res.error
-            );
-
-            const group_id = req.params.group_id;
-
-            const group = await req.db.groups.findOne({
-                where: {
-                    group_id,
-                },
-                raw: true,
-            });
-
-            if (!group) throw new res.error(404, "Group not found");
-
-            res.status(200).json({
-                ok: true,
-                message: "Group",
+                message: "Students list",
                 data: {
-                    group,
-                },
-            });
-        } catch (error) {
-            console.log(error);
-            next(error);
-        }
-    }
-
-    static async GroupGetAllController(req, res, next) {
-        try {
-            permissionChecker(
-                ["admin", "teacher", "operator"],
-                req.user_permissions,
-                res.error
-            );
-
-            const groups = await req.db.groups.findAll({
-                raw: true,
-            });
-
-            res.status(200).json({
-                ok: true,
-                message: "Groups list",
-                data: {
-                    groups,
+                    group_students,
                 },
             });
         } catch (error) {
