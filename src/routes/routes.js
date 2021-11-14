47dc0ca8a8c05930eb96c3e0d58570a65b0ca901
Javohir <javohir200903@gmail.com>
2021-11-14 19:47:41 +0500

    add GroupController

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 const errorHandler = require("../helpers/errorHandler");
 const ApplicantRoute = require("./applicants/ApplicantRoute");
 const CourseRoute = require("./courses/CourseRoute");
+const GroupRoute = require("./groups/GroupRoute");
 const TeacherRoute = require("./teachers/TeacherRoute");
 const UserRoute = require("./users/UserRoute");
 
@@ -10,6 +11,7 @@ module.exports = (app) => {
         app.use(TeacherRoute.path, TeacherRoute.router);
         app.use(CourseRoute.path, CourseRoute.router);
         app.use(ApplicantRoute.path, ApplicantRoute.router);
+        app.use(GroupRoute.path, GroupRoute.router);
     } finally {
         app.use(errorHandler);
     }
