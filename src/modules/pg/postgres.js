250774b4c5dfdd471202d269467ba86981f96500
Javohir <javohir200903@gmail.com>
2021-11-14 19:05:34 +0500

    add GropuModel

--- src/modules/pg/postgres.js
+++ src/modules/pg/postgres.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require("sequelize");
 const ApplicantModel = require("../../models/ApplicantModel");
 const CourseModel = require("../../models/CourseModel");
+const GroupModel = require("../../models/GroupModel");
 const PermissionModel = require("../../models/PermissionModel");
 const SessionModel = require("../../models/SessionModel");
 const TeacherModel = require("../../models/TeacherModel");
@@ -26,6 +27,7 @@ module.exports = async function postgres() {
         db.teachers = await TeacherModel(sequelize, Sequelize);
         db.courses = await CourseModel(sequelize, Sequelize);
         db.applicants = await ApplicantModel(sequelize, Sequelize);
+        db.groups = await GroupModel(sequelize, Sequelize);
 
         await relations(db);
 
